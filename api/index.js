dabd0946736ba1b48b2657e3ce0c623aa107ce1a
Lao R <laorivera@hotmail.com>
2025-12-13 18:42:54 -0300

    ??

--- api/index.js
+++ api/index.js
@@ -1,31 +1,30 @@
-
 import { ProductRoutes } from '../src/routes/product_routes.js';
 import { LoginRoutes } from '../src/routes/login_routes.js'; 
 import { Server } from '../src/server/server.js';
 
-async function main(){
+let cachedApp = null;
 
-    const server = new Server(); 
+async function main() {
+  const server = new Server(); 
     
-    const productRoutes = new ProductRoutes();
-    const loginRoutes = new LoginRoutes();
+  const productRoutes = new ProductRoutes();
+  const loginRoutes = new LoginRoutes();
 
-    server.addRoute('/api', productRoutes.init());
+  server.addRoute('/api', productRoutes.init());
+  server.addRoute('/auth', loginRoutes.init()); 
 
-    server.addRoute('/auth', loginRoutes.init()); 
-
-    return server.getApp();
+  return server.getApp();
 }
 
-const app = await main().catch(error => {
-    console.log('Server error:', error.message);
-    throw error;
-});
+export default async function handler(req, res) {
+  if (!cachedApp) {
+    cachedApp = await main();
+  }
+  return cachedApp(req, res);
+}
 
 
-export default app;
-
-
+/*
 
 if (process.env.VERCEL !== '1') {
     main().then(app => {
@@ -36,4 +35,4 @@ if (process.env.VERCEL !== '1') {
     });
 }
 
-
+*/
